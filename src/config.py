2b43b8110691e1844636f05d7495acaacf93cf8c
FidTCHANDO <fidson.tchando1998@gmail.com>
2023-11-28 18:36:04 +0100

    Test unit ok for src

--- src/config.py
+++ src/config.py
@@ -2,10 +2,10 @@ from __future__ import print_function
 import argparse
 
 parser = argparse.ArgumentParser(description='cmdArgs')
-parser.add_argument('--output', type=str, default='slack_data.csv',
+parser.add_argument('donnees.csv', type=str, default='slack_data.csv',
                 help='filename to write analysis output in CSV format')
-parser.add_argument('--path', required=True, type=str, help='directory where slack data reside')
-parser.add_argument('--channel', type=str, default='', help='which channel we parsing')
+parser.add_argument('C:\Users\TCHANDO\network_analysis\', required=True, type=str, help='directory where slack data reside')
+parser.add_argument('all-week5', type=str, default='', help='which channel we parsing')
 parser.add_argument('--userfile', type=str, default='users.json', help='users profile information')
 
 cfg = parser.parse_args()
